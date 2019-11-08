402c5a83e4296783ef9f62313492c491c902f467
Gil Desmarais <git@desmarais.de>
2019-11-08 19:03:38 +0100

    fix: ttl handled as seconds when it should be minutes

--- app.rb
+++ app.rb
@@ -12,17 +12,11 @@ class App < Sinatra::Base
   get '/health_check.txt' do
     content_type 'text/plain'
 
-    errors = []
+    broken_feeds = errors
 
-    yaml_feed_names.each do |feed_name|
-      Html2rss.feed_from_yaml_config(CONFIG_FILE, feed_name).to_s
-    rescue e
-      errors << "#{feed_name}: #{e.message}"
-    end
-
-    if errors.count.positive?
+    if broken_feeds.any?
       status 500
-      errors.join("\n")
+      broken_feeds.join("\n")
     else
       status 200
       'success'
@@ -32,7 +26,11 @@ class App < Sinatra::Base
   get '/*.rss' do
     feed_name = params[:splat].first
 
-    feed_config = yaml_feeds[feed_name] || Html2rss::Configs.find_by_name(feed_name, params)
+    feed_config = begin
+                    yaml_feeds[feed_name] || Html2rss::Configs.find_by_name(feed_name, params)
+                  rescue StandardError
+                    nil
+                  end
 
     feed_config ? respond_with_feed(feed_config) : status(404)
   end
@@ -44,7 +42,7 @@ class App < Sinatra::Base
     feed = Html2rss.feed(config)
 
     content_type 'text/xml'
-    expires config.ttl, :public, :must_revalidate
+    expires(config.ttl * 60, :public, :must_revalidate)
     items?(feed) ? feed.to_s : status(500)
   end
 
@@ -62,5 +60,15 @@ class App < Sinatra::Base
 
   def yaml_feed_names
     yaml_feeds.keys
+  end
+
+  def errors
+    [].tap do |errors|
+      yaml_feed_names.each do |feed_name|
+        Html2rss.feed_from_yaml_config(CONFIG_FILE, feed_name).to_s
+      rescue e
+        errors << "#{feed_name}: #{e.message}"
+      end
+    end
   end
 end
