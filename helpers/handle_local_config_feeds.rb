ab899e263c88a5abefb09e9cc54be9c6892dce3f
Gil Desmarais <git@desmarais.de>
2024-08-19 16:37:00 +0200

    feat: use content-type constant

--- helpers/handle_local_config_feeds.rb
+++ helpers/handle_local_config_feeds.rb
@@ -7,7 +7,7 @@ module Html2rss
         path = RequestPath.new(request)
 
         Html2rssFacade.from_local_config(path.full_config_name, typecast_params) do |config|
-          response['Content-Type'] = 'text/xml'
+          response['Content-Type'] = CONTENT_TYPE_RSS
           HttpCache.expires(response, config.ttl * 60, cache_control: 'public')
         end
       end
