e0ffd1e2f8893b402ea42ed07328bae5d0a9be9d
Gil Desmarais <git@desmarais.de>
2024-10-20 13:04:44 +0200

    test(auto_source): add specs for helpers

--- helpers/auto_source.rb
+++ helpers/auto_source.rb
@@ -13,8 +13,15 @@ module Html2rss
       def self.enabled?        = ENV['AUTO_SOURCE_ENABLED'].to_s == 'true'
       def self.username        = ENV.fetch('AUTO_SOURCE_USERNAME')
       def self.password        = ENV.fetch('AUTO_SOURCE_PASSWORD')
-      def self.allowed_origins = ENV.fetch('AUTO_SOURCE_ALLOWED_ORIGINS', '').split(',').to_set
 
+      def self.allowed_origins = ENV.fetch('AUTO_SOURCE_ALLOWED_ORIGINS', '')
+                                    .split(',')
+                                    .map(&:strip)
+                                    .reject(&:empty?)
+                                    .to_set
+
+      # @param encoded_url [String] Base64 encoded URL
+      # @return [RSS::Rss]
       def self.build_auto_source_from_encoded_url(encoded_url)
         url = Addressable::URI.parse Base64.urlsafe_decode64(encoded_url)
         request = SsrfFilter.get(url)
@@ -27,10 +34,16 @@ module Html2rss
         auto_source.build
       end
 
+      # @param rss [RSS::Rss]
+      # @param default_in_minutes [Integer]
+      # @return [Integer]
       def self.ttl_in_seconds(rss, default_in_minutes: 60)
-        (rss.channel.ttl || default_in_minutes) * 60
+        (rss&.channel&.ttl || default_in_minutes) * 60
       end
 
+      # @param request [Roda::RodaRequest]
+      # @param response [Roda::RodaResponse]
+      # @param allowed_origins [Set<String>]
       def self.check_request_origin!(request, response, allowed_origins = AutoSource.allowed_origins)
         if allowed_origins.empty?
           response.write 'No allowed origins are configured. Please set AUTO_SOURCE_ALLOWED_ORIGINS.'
@@ -38,7 +51,7 @@ module Html2rss
           origin = Set[request.env['HTTP_HOST'], request.env['HTTP_X_FORWARDED_HOST']].delete(nil)
           return if allowed_origins.intersect?(origin)
 
-          response.write "Origin '#{origin}' is not allowed."
+          response.write 'Origin is not allowed.'
         end
 
         response.status = 403
