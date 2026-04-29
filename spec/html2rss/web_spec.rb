49bc996f54e1c364f95beb2ba35c18f248fca398
Gil Desmarais <git@desmarais.de>
2026-04-29 20:59:00 +0200

    ci: harden release workflows

--- spec/html2rss/web_spec.rb
+++ spec/html2rss/web_spec.rb
@@ -7,7 +7,7 @@ require_relative '../../config/version'
 RSpec.describe Html2rss::Web do
   describe 'VERSION' do
     it 'defines the canonical application release version' do
-      expect(described_class::VERSION).to eq('1.0.0')
+      expect(described_class::VERSION).to match(/\A\d+\.\d+\.\d+(?:-[0-9A-Za-z.-]+)?(?:\+[0-9A-Za-z.-]+)?\z/)
     end
   end
 end
